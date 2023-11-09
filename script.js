7905f2d370a8db02683b3a5a343b1098a2dcb5a5
adamwofford <asw497@msstate.edu>
2023-11-08 22:49:34 -0600

    progressed until part of the way through exp.

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 // Open your website in your browser, go to developer tools 
 // and open the console. 
 // You should see "Hello from Javascript written in the console."
-console.log("Hello from Javascript"); // DELETE ME 
 
 // ADD YOUR toggleMenu() function here
+function toggleMenu()
+{
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 
+}
 // CODE HERE
 
 // END
